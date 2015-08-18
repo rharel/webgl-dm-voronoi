1c3c066ff99fdc78be5a14ef9f0363cfaa00c58f
Raoul Harel <raoulhaa@gmail.com>
2015-08-18 21:22:22 +0200

    Added tent geometry generation

--- src/geometry-extensions.js
+++ src/geometry-extensions.js
@@ -48,5 +48,57 @@ var GeometryExtensions = {
     geometry.mergeVertices();  // i.e. if angle is 2pi, removes 1 duplicate
 
     return geometry;
+  },
+
+  /**
+   * Generates a tent geometry with given width, height and depth.
+   *
+   * @details
+   *  Call the parameter width 'w', height 'h' and depth 'd'.
+   *
+   *  The edge forming the tent-top is centered at the origin.
+   *  The tent is h units long on the y-axis, and w units wide on the x-axis.
+   *  The tent extrudes towards the negative z-axis, up until z = -d
+   *
+   * @returns {THREE.Geometry}
+   */
+  tent: function(width, height, depth) {
+
+    var geometry = new THREE.Geometry();
+
+    // vertices //
+
+    var h2 = height / 2;
+    var w2 = width / 2;
+
+    geometry.vertices = [
+
+      new THREE.Vector3(0, -h2, 0),
+      new THREE.Vector3(0,  h2, 0),
+
+      new THREE.Vector3(-w2, -h2, -depth),  // LB
+      new THREE.Vector3(-w2,  h2, -depth),  // LT
+
+      new THREE.Vector3( w2, -h2, -depth),  // RB
+      new THREE.Vector3( w2,  h2, -depth)   // RT
+    ];
+
+    geometry.verticesNeedUpdate = true;
+
+    // faces //
+
+    // B = 0, T = 1, LB = 2, LT = 3, RB = 4, RT = 5;
+    geometry.faces = [
+
+      new THREE.Face3(0, 3, 2),
+      new THREE.Face3(0, 1, 3),
+
+      new THREE.Face3(0, 4, 5),
+      new THREE.Face3(0, 5, 1)
+    ];
+
+    geometry.elementsNeedUpdate = true;
+
+    return geometry;
   }
 };
