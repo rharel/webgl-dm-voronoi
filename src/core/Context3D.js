2a682d09ae88edc3a6667614a90ed656d9b0c66c
Raoul Harel <raoulhaa@gmail.com>
2015-08-20 18:17:13 +0200

    The far rendering is now dependent on canvas dimensions
    This fixes a bug where distance meshes where too large to fit in the camera frustum

--- src/core/Context3D.js
+++ src/core/Context3D.js
@@ -14,7 +14,7 @@ function Context3D(canvas) {
   this._camera = new THREE.OrthographicCamera(
     0, canvas.width,
     canvas.height, 0,
-    1, 1000
+    1, Math.max(canvas.width, canvas.height)
   );
   this._camera.position.z = 2;
 
@@ -30,7 +30,22 @@ Context3D.prototype = {
 
   add: function(mesh) { this._scene.add(mesh); },
 
+  remove: function(mesh) { this._scene.remove(mesh); },
+
   render: function() { this._renderer.render(this._scene, this._camera); },
+
+  resize: function() {
+
+    this._renderer.setSize(
+      this._canvas.width,
+      this._canvas.height
+    );
+
+    this._camera.right = this._canvas.width;
+    this._camera.top = this._canvas.height;
+    this._camera.far = Math.max(this._canvas.width, this._canvas.height) + 1;
+    this._camera.updateProjectionMatrix();
+  },
 
   material: function(color) {
 
