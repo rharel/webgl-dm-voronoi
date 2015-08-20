4fd759d72afa45e3a9a4a111a09b852310277fc3
Raoul Harel <raoulhaa@gmail.com>
2015-08-20 18:22:42 +0200

    Added demo showcasing the library

---
+++ demo/js/Random.js
@@ -0,0 +1,29 @@
+/**
+ * @author Raoul Harel
+ * @license The MIT license (LICENSE.txt)
+ * @copyright 2015 Raoul Harel
+ * @url https://github.com/rharel/webgl-dm-voronoi
+ */
+
+var Random = {
+
+  inRange: function (a, b) {
+    return a + Math.random() * (b - a);
+  },
+
+  color: (function () {
+
+    function mix(a, b) {
+      return (a + b) / 2;
+    }
+
+    return function () {
+
+      return {
+        r: mix(Math.random(), 1),
+        g: mix(Math.random(), 1),
+        b: mix(Math.random(), 1)
+      };
+    };
+  })()
+};
